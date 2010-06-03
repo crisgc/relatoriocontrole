889e32a4c8980be73f1b87c6058caf4dd2e21ef9
diogolr <diogolr@8af06cec-306b-3d57-b2f1-e8df45151593>
2010-06-03 14:41:31 +0000

    Adição dos arquivos de referência bibliográfica, correção da figura da questão 4 e adição dos apêndices.

---
+++ lista_1/scripts_matlab/q3.m
@@ -0,0 +1,10 @@
+% Universidade Federal do Rio Grande do Norte
+% Programa de Pos-Graduacao em Engenharia Eletrica e de Computacao
+% Lista 1 - Questao 3
+% Autores: Anna Giselle Camara Dantas Ribeiro
+%          Cristiano Gurgel de Castro
+%          Diogo Leite Reboucas
+%          Thiago Medeiros Barros
+
+clear;
+clc;
