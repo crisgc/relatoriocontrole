4df3d74a3a74f6779b39c15a7709cb254ad5ab5a
diogolr <diogolr@8af06cec-306b-3d57-b2f1-e8df45151593>
2010-06-07 22:08:27 +0000

    Adição das imagens da resposta e o diagrama de blocos da simulação da questão 3. Faltando definir corretamente as posições e comentar sobre os resultados.

--- lista_1/scripts_matlab/q3.m
+++ lista_1/scripts_matlab/q3.m
@@ -8,3 +8,64 @@
 
 clear;
 clc;
+
+a = 1;
+b = 1;
+c = 1;
+
+a_m = 2;
+b_m = 1;
+c_m = 1;
+
+gammas = [0.1 0.2 0.5 0.7 1.0];
+cores = [ 'b', 'g', 'r', 'c', 'm' ];
+
+periodo_ref = 200;
+tempo_simulacao = 1000;
+
+tam = length( gammas );
+
+% Guardando as saidas
+saidas = cell( tam, 1 );
+
+for i = 1 : tam
+    gamma = gammas( i );
+    
+    open( 'q3_simulacao' );
+    sim( 'q3_simulacao' );
+    
+    saidas{i} = saida;
+end
+
+% Plot das saidas
+for i = 1 : tam
+    figure;    
+    
+    % Referencia + Saida
+    subplot( 3, 1, 1 );
+    ylabel( 'Saida' );
+    hold on;
+    grid on;
+    plot( saidas{i}( :, 1 ), saidas{i}( :, 4 ), 'k--' );
+    plot( saidas{i}( :, 1 ), saidas{i}( :, 3 ), 'b' );
+    legend( 'Referencia', 'Saida' );
+    
+    % Erro + Sinal de Controle
+    subplot( 3, 1, 2 );
+    ylabel( 'Erro / Sinal de Controle' );
+    hold on;
+    grid on;
+    plot( saidas{i}( :, 1 ), saidas{i}( :, 2 ), 'r' );
+    plot( saidas{i}( :, 1 ), saidas{i}( :, 5 ), 'b' );
+    legend( 'Erro', 'Sinal de Controle' );
+    
+    % Evolucao Theta
+    subplot( 3, 1, 3 );
+    ylabel( 'Valores de \theta' );
+    hold on;
+    grid on;
+    plot( saidas{i}( :, 1 ), saidas{i}( :, 6 ), 'r' );
+    plot( saidas{i}( :, 1 ), saidas{i}( :, 7 ), 'g' );
+    plot( saidas{i}( :, 1 ), saidas{i}( :, 8 ), 'b' );
+    legend( '\theta_1', '\theta_2', '\theta_3' );
+end
